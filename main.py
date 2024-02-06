e96f2b1c5b8645f83fb97ff407c2b131693b29f7
Harry <harry.yefan.zhou@gmail.com>
2024-02-06 13:17:53 +1100

    Added shutdown capability, converted into a bot entity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import dotenv
 import os
 
@@ -13,22 +14,36 @@ intents.messages = True
 intents.presences = True
 intents.message_content = True
 
-# Start the discord client
-client = discord.Client(intents=intents)
+# Intialize the discord bot with the defined intents
+bot = commands.Bot(command_prefix = '/', intents = intents)
 
 # Log if the discord bot has logged in correctly
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'Logged in as {bot.user}')
+
 
 # Test function ("Hello")
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     
-    if message.content.startswith('$hello'):
+    if message.content.startswith('Hello'):
         await message.channel.send('Hello!')
 
+    # Need to process commands to avoid breaking command functionality
+    await bot.process_commands(message)
+
+# Allow owner/admin to shutdown the bot
+@bot.command(name='shutdown')
+@commands.is_owner()
+async def shutdown(ctx):
+    # Send a message indicating that the bot is shutting down
+    await ctx.send('Shutting down TeamPicker!')
+    await bot.close()
+    print("Bot has been logged out")
+
 # Run the discord bot
-client.run(token)
+bot.run(token)
+
