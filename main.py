1148f55fb3822ea05745d77f2a265f0ef631f4d7
Harry <harry.yefan.zhou@gmail.com>
2024-02-07 23:17:59 +1100

    Implemented basic structure for map picking

--- main.py
+++ main.py
@@ -23,6 +23,14 @@ intents.guilds = True
 # Intialize the discord bot with the defined intents
 bot = commands.Bot(command_prefix = '/', intents = intents)
 
+# Store Global Captain variables for future use
+bot.electedCaptainA = None
+bot.electedCaptainB = None
+
+# Store MapData
+
+CS_MAPS = ['Dust 2', 'Mirage', 'Inferno', 'Cache', 'Ancient']
+
 """
 Function that alerts the administrator that the bot has logged in correctly
 """
@@ -31,7 +39,8 @@ async def on_ready():
     print(f'Logged in as {bot.user}')
 
 """
-Function that replies "Hello!" in the chat from the bot if any user writes Hello into the chat
+Function that handles all potential message commands sent into the chat
+    - If Hello is typed in the chat, the bot will respond with Hello! back
 """
 @bot.event
 async def on_message(message):
@@ -40,6 +49,16 @@ async def on_message(message):
     
     if message.content.startswith('Hello'):
         await message.channel.send('Hello!')
+
+    if message.author == bot.electedCaptainA:
+        if message.content.startswith('pick'):
+            mapChosen = False
+            mapsAvailable = CS_MAPS.copy()
+            while mapChosen == False:
+                print("Reached here")
+                await message.channel.send("Picked map!")
+                await bot.process_commands(message)
+                return
 
     # Need to process commands to avoid breaking command functionality
     await bot.process_commands(message)
@@ -59,6 +78,7 @@ async def shutdown(ctx):
 Function that selects 2 team captains from a channel that the user is in if they type in the command.
 If the user is not in a channel, the user will be prompted to join a channel.
 If the user is in a channel, but there is only one user in that specific channel, the user will be mildly insulted.
+Return - The two elected capatins
 """
 @bot.command(name='elect')
 async def electCaptain(ctx):
@@ -66,14 +86,29 @@ async def electCaptain(ctx):
     if curChannel:
         members = functions.findMembers(curChannel)
         # Check if channel has at least 2 members
-        if len(members) >= 2:
-            electedCaptainA, electedCaptainB = random.sample(members, 2)
-            await ctx.send(f'{electedCaptainA.display_name} has been elected as the captain of Alpha team!')
-            await ctx.send(f'{electedCaptainB.display_name} has been elected as the captain of Bravo team!')
-        else:
-            await ctx.send('You need to get at least one friend before you elect team captains :(')
+        # @TODO UNCOMMENT THIS LATER, KEEP IT COMMENTED FOR TESTING PURPOSES
+        # if len(members) >= 2:
+        #     bot.electedCaptainA, bot.electedCaptainB = random.sample(members, 2)
+        #     await ctx.send(f'{bot.electedCaptainA.display_name} has been elected as the captain of Alpha team!')
+        #     await ctx.send(f'{bot.electedCaptainB.display_name} has been elected as the captain of Bravo team!')
+        # else:
+        #     await ctx.send('You need to get at least one friend before you elect team captains :(')
+        bot.electedCaptainA = members[0]
+        await ctx.send(f'{bot.electedCaptainA.display_name} has been elected as the captain of Alpha team!')
     else:
         await ctx.send('You must first join a channel before you can use this command!')
+
+    
+    
+
+                
+
+
+
+"""
+Function that automates the whole team picking process, if possible, elect two captains and then allow those two captains to select team members. Teams are of size 10 for games like CSGO, DOTA2 and OVERWATCH.
+A randomly selected captain will be asked to flip a coin. If their guess is correct, they can choose to pick first or second.
+"""
 
 # Run the discord bot
 bot.run(token)
