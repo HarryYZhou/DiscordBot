307c2ff706e8ada380c2644c32f4843a5c9be98a
Harry <harry.yefan.zhou@gmail.com>
2024-02-09 18:13:57 +1100

    Implemented Pick Phase initiation logic

--- main.py
+++ main.py
@@ -26,10 +26,13 @@ bot = commands.Bot(command_prefix = '/', intents = intents)
 # Store Global Captain variables for future use
 bot.electedCaptainA = None
 bot.electedCaptainB = None
+bot.firstPicker = False
+bot.secondPicker = False
+bot.pickMaps = False
 
 # Store MapData
 
-CS_MAPS = ['Dust 2', 'Mirage', 'Inferno', 'Cache', 'Ancient']
+CS_MAPS = ['Dust 2', 'Mirage', 'Inferno', 'Cache', 'Ancient', 'Anubis', 'Office', 'Vertigo', 'Overpass', 'Nuke']
 
 """
 Function that alerts the administrator that the bot has logged in correctly
@@ -47,18 +50,21 @@ async def on_message(message):
     if message.author == bot.user:
         return
     
+    # Bot says hello
     if message.content.startswith('Hello'):
         await message.channel.send('Hello!')
 
-    if message.author == bot.electedCaptainA:
-        if message.content.startswith('pick'):
-            mapChosen = False
-            mapsAvailable = CS_MAPS.copy()
-            while mapChosen == False:
-                print("Reached here")
-                await message.channel.send("Picked map!")
-                await bot.process_commands(message)
-                return
+    # Picking map initiated if captains are elected
+    if bot.pickMaps == True:
+        if message.author == bot.electedCaptainA:
+            if message.content.startswith('pick'):
+                mapChosen = False
+                mapsAvailable = CS_MAPS.copy()
+                while mapChosen == False:
+                    print("Reached here")
+                    await message.channel.send("Picked map!")
+                    await bot.process_commands(message)
+                    return
 
     # Need to process commands to avoid breaking command functionality
     await bot.process_commands(message)
@@ -93,15 +99,48 @@ async def electCaptain(ctx):
         #     await ctx.send(f'{bot.electedCaptainB.display_name} has been elected as the captain of Bravo team!')
         # else:
         #     await ctx.send('You need to get at least one friend before you elect team captains :(')
+        # @TODO remove line below this
         bot.electedCaptainA = members[0]
         await ctx.send(f'{bot.electedCaptainA.display_name} has been elected as the captain of Alpha team!')
     else:
         await ctx.send('You must first join a channel before you can use this command!')
 
     
-    
+ 
+"""
+Function that flips a coin, used to determine who gets to pick for first/second pick
+"""
+@bot.command(name='coinflip')
+async def coinFlip(ctx):
+    await ctx.send("Flipping coin...")
+    await ctx.send(random.choice(('Flipped Heads!', 'Flipped Tails!')))
 
+"""
+Function that initiates the map picking phase if two captains have been appointed.
+Depending on which captain types this command, they will be able to go first. The /coinflip command can be used in conjunction with this to determine who goes first and second.
+"""
+@bot.command(name='pickmap')
+async def pickMaps(ctx):
+    if not bot.electedCaptainA and not bot.electedCaptainB:
+        await ctx.send("You must first elect team captains! You can do this by typing in /elect in the chat.")
+        return
+    else:
+        if ctx.author == bot.electedCaptainA or ctx.author == bot.electedCaptainB:
+            bot.pickMaps == True
+            bot.firstPicker = ctx.author
+            # Assign firstPicker and secondPicker depending on who types in the /pick command
+            if ctx.author == bot.electedCaptainA:
+                bot.secondPicker == bot.electedCaptainB
+            else:
+                bot.secondPicker == bot.selectedCaptainA
+            messageToSend = f"Captain {ctx.author.display_name}, please ban 3 maps from the following list (To ban a map, type the numbers in one by one):\n"
+            for i in range(len(CS_MAPS)):
                 
+                messageToSend += f"{i+1}. {CS_MAPS[i]}\n"
+            await ctx.send(messageToSend)
+
+
+
 
 
 
